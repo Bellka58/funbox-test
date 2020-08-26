5cf1696cf8e45eb48b25a2be4ba3458515886c45
Анастасия Белова <bellka58@gmail.com>
2020-08-26 16:26:40 +0300

    Updates

---
+++ precache-manifest.6f7125883ec4791338c60ef0be81213f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e1e0562c82447333df4750aa366f28",
+    "url": "/funbox-test/index.html"
+  },
+  {
+    "revision": "2dfd14d7352ab0678d50",
+    "url": "/funbox-test/static/css/main.f19e2fcc.chunk.css"
+  },
+  {
+    "revision": "59c750b8481e40b72c1b",
+    "url": "/funbox-test/static/js/2.64e54c3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funbox-test/static/js/2.64e54c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dfd14d7352ab0678d50",
+    "url": "/funbox-test/static/js/main.26fda3e0.chunk.js"
+  },
+  {
+    "revision": "012fbde4df3c17e8fe22",
+    "url": "/funbox-test/static/js/runtime-main.59997f6a.js"
+  },
+  {
+    "revision": "f17bf942114c511e6d2c6c97932b3b1c",
+    "url": "/funbox-test/static/media/Pattern.f17bf942.png"
+  },
+  {
+    "revision": "edd4c00255a21484fb6f32202456d154",
+    "url": "/funbox-test/static/media/Photo.edd4c002.png"
+  }
+]);
